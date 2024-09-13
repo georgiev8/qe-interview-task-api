3582dae45e4d8f9cfafd5528d6318d9ddb00a40d
georgiev8 <street3r.georgiev8@gmail.com>
2024-09-13 22:37:44 +0300

    refactor the todo tests file.

--- src/test/groovy/requests/ToDoTests.groovy
+++ src/test/groovy/requests/ToDoTests.groovy
@@ -1,17 +1,17 @@
 package requests
 
+import resources.ToDo
+import org.junit.jupiter.api.Test
 import io.restassured.response.Response
 
-import org.junit.jupiter.api.Test
-import resources.ToDo
+class ToDoTests extends ToDo {
 
-class ToDoTests extends ToDo{
+    @Test
+    void test_createUserToDo() {
+        Response response = createToDo(1, "test todo title", false)
 
-  @Test
-  void test_createUserToDo() {
+        response.statusCode() == 201
+        response.prettyPrint()
+    }
 
-    Response response = createToDo(1, "Test Title", false)
-    response.statusCode() == 200
-    response.prettyPrint()
-  }
 }
