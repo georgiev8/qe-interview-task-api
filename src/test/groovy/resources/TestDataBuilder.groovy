2d759765fcc0dc263a775f4df86ac7ee1bc4ecbc
georgiev8 <street3r.georgiev8@gmail.com>
2024-09-13 22:36:59 +0300

    implement resource builder methods.

--- src/test/groovy/resources/TestDataBuilder.groovy
+++ src/test/groovy/resources/TestDataBuilder.groovy
@@ -2,44 +2,79 @@ package resources
 
 class TestDataBuilder {
 
-  /**
-   * In this class you will have to write methods that will returns the objects that you want to use in your requests
-   */
+    /** <--- userToDo builder method ---> */
+    static String userToDo(int userId, String title, boolean completed) {
+        return "  {\n" +
+                "    \"userId\": " + userId + ",\n" +
+                "    \"title\": \"" + title + "\",\n" +
+                "    \"completed\": " + completed + "\n" +
+                "  }"
+    }
 
+    /** <--- user builder method ---> */
+    static String user(String name, String userName, String email) {
+        return " {\n" +
+                "    \"name\": " + name + ",\n" +
+                "    \"username\": " + userName + ",\n" +
+                "    \"email\": " + email + ",\n" +
+                "    \"address\": {\n" +
+                "      \"street\": \"Kulas Light\",\n" +
+                "      \"suite\": \"Apt. 556\",\n" +
+                "      \"city\": \"Gwenborough\",\n" +
+                "      \"zipcode\": \"92998-3874\",\n" +
+                "      \"geo\": {\n" +
+                "        \"lat\": \"-37.3159\",\n" +
+                "        \"lng\": \"81.1496\"\n" +
+                "      }\n" +
+                "    },\n" +
+                "    \"phone\": \"1-770-736-8031 x56442\",\n" +
+                "    \"website\": \"hildegard.org\",\n" +
+                "    \"company\": {\n" +
+                "      \"name\": \"Romaguera-Crona\",\n" +
+                "      \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
+                "      \"bs\": \"harness real-time e-markets\"\n" +
+                "    }\n" +
+                "  }"
+    }
 
-  static String userToDo(int userId, String title, boolean completed) {
-    return "  {\n" +
-      "    \"userId\": " + userId + ",\n" +
-      "    \"title\": \"" + title + "\",\n" +
-      "    \"completed\": " + completed + "\n" +
-      "  }"
-  }
+    /** <--- photo builder method ---> */
+    static String photo(int albumId, int id, String title, String url, String thumbnailUrl) {
+        return " {\n" +
+                "    \"albumId\": " + albumId + ",\n" +
+                "    \"id\": " + id + ",\n" +
+                "    \"title\": \"" + title + "\",\n" +
+                "    \"url\": \"" + url + "\",\n" +
+                "    \"thumbnailUrl\": \"" + thumbnailUrl + "\"\n" +
+                "  }"
+    }
 
-  //Complete the user like the ToDos above
+    /** <--- album builder method ---> */
+    static String album(int userId, int id, String title) {
+        return " {\n" +
+                "    \"userId\": " + userId + ",\n" +
+                "    \"id\": " + id + ",\n" +
+                "    \"title\": \"" + title + "\"\n" +
+                "  }"
+    }
 
-  static String user(String name, String userName, String email) {
-    return " {\n" +
-      "    \"name\": " + name + ",\n" +
-      "    \"username\": " + userName + ",\n" +
-      "    \"email\": " + email + ",\n" +
-      "    \"address\": {\n" +
-      "      \"street\": \"Kulas Light\",\n" +
-      "      \"suite\": \"Apt. 556\",\n" +
-      "      \"city\": \"Gwenborough\",\n" +
-      "      \"zipcode\": \"92998-3874\",\n" +
-      "      \"geo\": {\n" +
-      "        \"lat\": \"-37.3159\",\n" +
-      "        \"lng\": \"81.1496\"\n" +
-      "      }\n" +
-      "    },\n" +
-      "    \"phone\": \"1-770-736-8031 x56442\",\n" +
-      "    \"website\": \"hildegard.org\",\n" +
-      "    \"company\": {\n" +
-      "      \"name\": \"Romaguera-Crona\",\n" +
-      "      \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
-      "      \"bs\": \"harness real-time e-markets\"\n" +
-      "    }\n" +
-      "  }"
-  }
+    /** <--- comment builder method ---> */
+    static String comment(int postId, int id, String name, String email, String body) {
+        return " {\n" +
+                "    \"postId\": " + postId + ",\n" +
+                "    \"id\": " + id + ",\n" +
+                "    \"name\": \"" + name + "\",\n" +
+                "    \"email\": \"" + email + "\",\n" +
+                "    \"body\": \"" + body + "\"\n" +
+                "  }"
+    }
 
+    /** <--- post builder method ---> */
+    static String post(int userId, int id, String title, String body) {
+        return " {\n" +
+                "    \"userId\": " + userId + ",\n" +
+                "    \"id\": " + id + ",\n" +
+                "    \"title\": \"" + title + "\",\n" +
+                "    \"body\": \"" + body + "\"\n" +
+                "  }"
+    }
 }
