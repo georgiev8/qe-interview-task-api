345225859170944e81f11699c6562e9ee2585b8e
georgiev8 <street3r.georgiev8@gmail.com>
2024-09-14 17:06:03 +0300

    refactor the testBuilder by using string interpolation.

--- src/test/groovy/resources/TestDataBuilder.groovy
+++ src/test/groovy/resources/TestDataBuilder.groovy
@@ -4,77 +4,77 @@ class TestDataBuilder {
 
     /** <--- userToDo builder method ---> */
     static String userToDo(int userId, String title, boolean completed) {
-        return "  {\n" +
-                "    \"userId\": " + userId + ",\n" +
-                "    \"title\": \"" + title + "\",\n" +
-                "    \"completed\": " + completed + "\n" +
-                "  }"
+        return """{
+            "userId": $userId,
+            "title": "$title",
+            "completed": $completed
+        }"""
     }
 
     /** <--- user builder method ---> */
     static String user(String name, String userName, String email) {
-        return " {\n" +
-                "    \"name\": " + name + ",\n" +
-                "    \"username\": " + userName + ",\n" +
-                "    \"email\": " + email + ",\n" +
-                "    \"address\": {\n" +
-                "      \"street\": \"Kulas Light\",\n" +
-                "      \"suite\": \"Apt. 556\",\n" +
-                "      \"city\": \"Gwenborough\",\n" +
-                "      \"zipcode\": \"92998-3874\",\n" +
-                "      \"geo\": {\n" +
-                "        \"lat\": \"-37.3159\",\n" +
-                "        \"lng\": \"81.1496\"\n" +
-                "      }\n" +
-                "    },\n" +
-                "    \"phone\": \"1-770-736-8031 x56442\",\n" +
-                "    \"website\": \"hildegard.org\",\n" +
-                "    \"company\": {\n" +
-                "      \"name\": \"Romaguera-Crona\",\n" +
-                "      \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
-                "      \"bs\": \"harness real-time e-markets\"\n" +
-                "    }\n" +
-                "  }"
+        return """{
+            "name": "$name",
+            "username": "$userName",
+            "email": "$email",
+            "address": {
+                "street": "Kulas Light",
+                "suite": "Apt. 556",
+                "city": "Gwenborough",
+                "zipcode": "92998-3874",
+                "geo": {
+                    "lat": "-37.3159",
+                    "lng": "81.1496"
+                }
+            },
+            "phone": "1-770-736-8031 x56442",
+            "website": "hildegard.org",
+            "company": {
+                "name": "Romaguera-Crona",
+                "catchPhrase": "Multi-layered client-server neural-net",
+                "bs": "harness real-time e-markets"
+            }
+        }"""
     }
 
     /** <--- photo builder method ---> */
     static String photo(int albumId, int id, String title, String url, String thumbnailUrl) {
-        return " {\n" +
-                "    \"albumId\": " + albumId + ",\n" +
-                "    \"id\": " + id + ",\n" +
-                "    \"title\": \"" + title + "\",\n" +
-                "    \"url\": \"" + url + "\",\n" +
-                "    \"thumbnailUrl\": \"" + thumbnailUrl + "\"\n" +
-                "  }"
+        return """{
+            "albumId": $albumId,
+            "id": $id,
+            "title": "$title",
+            "url": "$url",
+            "thumbnailUrl": "$thumbnailUrl"
+        }"""
     }
 
     /** <--- album builder method ---> */
     static String album(int userId, int id, String title) {
-        return " {\n" +
-                "    \"userId\": " + userId + ",\n" +
-                "    \"id\": " + id + ",\n" +
-                "    \"title\": \"" + title + "\"\n" +
-                "  }"
+        return """{
+            "userId": $userId,
+            "id": $id,
+            "title": "$title"
+        }"""
     }
 
     /** <--- comment builder method ---> */
     static String comment(int postId, int id, String name, String email, String body) {
-        return " {\n" +
-                "    \"postId\": " + postId + ",\n" +
-                "    \"id\": " + id + ",\n" +
-                "    \"name\": \"" + name + "\",\n" +
-                "    \"email\": \"" + email + "\",\n" +
-                "    \"body\": \"" + body + "\"\n" +
-                "  }"
+        return """{
+            "postId": $postId,
+            "id": $id,
+            "name": "$name",
+            "email": "$email",
+            "body": "$body"
+        }"""
     }
 
     /** <--- post builder method ---> */
     static String post(int userId, int id, String title, String body) {
-        return " {\n" +
-                "    \"userId\": " + userId + ",\n" +
-                "    \"id\": " + id + ",\n" +
-                "    \"title\": \"" + title + "\",\n" +
-                "    \"body\": \"" + body + "\"\n" +
-                "  }"
+        return """{
+            "userId": $userId,
+            "id": $id,
+            "title": "$title",
+            "body": "$body"
+        }"""
     }
 }
